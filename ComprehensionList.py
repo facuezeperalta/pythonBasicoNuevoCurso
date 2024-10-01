2f4ff064e9330ec3a204dbd6b1ff94ac5b405a21
facuezeperalta <facundoperaltaezequiel@gmail.com>
2024-10-01 15:15:11 -0300

    Comprehension list done.

--- ComprehensionList.py
+++ ComprehensionList.py
@@ -2,4 +2,11 @@ squares = [x ** 2 for x in range(1,11)]
 #print("Los cuadrados son: ", squares)
 
 celcius = [0,10,20,30,40,50]
-fahrenheit = [(9/5 * t) +32   ]
+fahrenheit = [(9/5 * t) +32 for t in celcius]
+#print("temperatura en Fahrenheit: ", fahrenheit)
+
+#Numeros pares
+evens = [x for x in range(1,21) if x % 2 == 0]
+print("Números pares: ", evens)
+unevens = [x for x in range (1,21) if x % 2 != 0]
+print("Números impares: ", unevens)
