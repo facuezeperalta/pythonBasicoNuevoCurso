8c70c24d19d1b17afdf2aa6feb9e31064252b9d4
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-02 19:30:02 -0300

    funciones done.

--- funciones.py
+++ funciones.py
@@ -1,5 +1,38 @@
-def greeting(name):
+""" def greeting(name):
     print(f"hola, {name}")
 
 
-greeting("facu")
+greeting("facu") """
+
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def calculator():
+    while True:
+        print("Selecciones una operación: ")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Division")
+        print("4. Multiplicacion")
+        print("0. Exit")
+
+        option = int(input("Ingrese una opción (1,2,3,4,0): "))
+
+        if option == 0:
+          print("Saliendo de la calculadora")
+        
+        if option in [1,2,3,4]:
+            num1 = float("Ingresa un número: ")
+            num2 = float("Ingresa el segundo numero: ")
+            
+
+
