45c03ca7b3cf1a1d9b301bbcbb2bed8d13cac750
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-03 21:37:18 -0300

    arboles de excepciones.

--- exceptions.py
+++ exceptions.py
@@ -2,8 +2,19 @@ try:
     divisor = int(input("ingresa un numero divisor: "))
     result = 100 / divisor
     print(result)
-except ZeroDivisionError:
+except ZeroDivisionError as e:
     print("El divisor no puede ser 0")
+    print("Nombre del error: ", e)
 except ValueError:
     print("Debes ingresar un número!")
    
+
+
+
+#Arbol de excepciones:
+def print_exception_hierarchy(exception_class, indent=0):
+    print(' ' * indent + exception_class.__name__)
+    for subclass in exception_class.__subclasses__():
+        print_exception_hierarchy(subclass, indent + 4)
+
+print_exception_hierarchy(Exception)
