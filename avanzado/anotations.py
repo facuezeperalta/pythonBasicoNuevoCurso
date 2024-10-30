67c8242a110d91b52fc716c375b95c20c88db25d
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-29 21:12:02 -0300

    anotations start.

---
+++ avanzado/anotations.py
@@ -0,0 +1,9 @@
+#Ids de empleados:
+id1: int = 101
+id2: int = 102
+
+#sumar id´s
+total_id = id1 + id2
+
+#mostramos el resultado
+print(total_id)
---
+++ avanzado/innertOuter.py
@@ -0,0 +1,15 @@
+x = 'global' #variable global.
+
+#función externa
+def outer_function():
+    x = "enclosing"
+    #función interna
+    def inner_function():
+        x = 'local' #variable local.
+        print(x)
+    
+    inner_function()
+    print(x)
+
+outer_function()
+print(x)
